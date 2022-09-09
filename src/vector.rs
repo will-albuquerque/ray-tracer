78f7bff19144e1784ab27305920038458c703471
William Albuquerque <w.j.albuquerque@outlook.com>
2022-09-09 19:07:23 +0100

    Add Vector

--- src/vector.rs
+++ src/vector.rs
@@ -1,1 +1,9 @@
+/// A three dimensional vector.
+pub struct Vector([f64; 3]);
 
+impl Vector {
+    /// Creates a new `Vector`.
+    pub fn new(inner: [f64; 3]) -> Vector {
+        Vector(inner)
+    }
+}
